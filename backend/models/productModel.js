0e953a4756e3a0770ccb64c0dca5e30c9989cbf5
Anjesh Kafle <anjeshkafle@gmail.com>
2024-04-04 06:18:24 +0100

    product model updated

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -2,37 +2,40 @@ const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
+// Unit Price Schema
+const unitPriceSchema = new Schema({
+  grams: { type: Number, required: true, min: 1 }, // Minimum of 1g
+  price: { type: Number, required: true, min: 0 }, // Price for the specified grams
+});
+
 // Price History Schema
 const priceHistorySchema = new Schema({
-  price: { type: Number, required: true },
+  unitPrices: [unitPriceSchema], // Now tracks changes to unit pricing
   date: { type: Date, default: Date.now },
-  user: { type: Schema.Types.ObjectId, ref: "User", required: true }, // Reference to the User who made the price change
+  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
 });
 
 // Stock Transaction Schema
 const stockTransactionSchema = new Schema({
   type: { type: String, enum: ["add", "remove", "override"], required: true },
-  quantity: { type: Number, required: true },
+  quantity: { type: Number, required: true }, // This could be in grams to match the unit of measurement
   date: { type: Date, default: Date.now },
   note: String, // Optional note for context on the transaction
-  user: { type: Schema.Types.ObjectId, ref: "User", required: true }, // Reference to the User who made the transaction
+  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
 });
 
 // Product Schema
 const productSchema = new Schema({
   name: { type: String, required: true },
   description: String,
-  strain: { type: String, required: true }, // e.g., Indica, Sativa, Hybrid
-  THCContent: { type: Number, required: true }, // THC percentage
-  CBDContent: { type: Number, required: true }, // CBD percentage
-  terpenes: [String], // Common terpenes for flavor and effect
-  price: { type: Number, required: true }, // Current price
-  priceHistory: [priceHistorySchema], // Track price changes over time
-  stock: { type: Number, required: true, min: 0 }, // Current stock
-  stockTransactions: [stockTransactionSchema], // Track stock changes
-  isHouseProduct: { type: Boolean, default: false }, //if false product belongs to a club otherwise superadmin
-  ownerClub: { type: Schema.Types.ObjectId, ref: "Club" },
-  listed: { type: Boolean, default: false },
+  strain: { type: String, required: true },
+  THCContent: { type: Number, required: true },
+  CBDContent: { type: Number, required: true },
+  terpenes: [String],
+  unitPrices: [unitPriceSchema], // Defines available quantities and their prices
+  priceHistory: [priceHistorySchema],
+  stock: { type: Number, required: true, min: 0 },
+  stockTransactions: [stockTransactionSchema],
 });
 
 module.exports = mongoose.model("Product", productSchema);
